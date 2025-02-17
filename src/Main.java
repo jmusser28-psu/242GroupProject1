237a1b8d97570438b04729fd85cc54418d0ec90d
jmusser28-psu <jmusser28@psu.edu>
2025-02-17 13:45:55 -0500

    Initial set up

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
 public class Main {
-    public static void printLetter() {
+    public static void printLetter(String offense, String numberOfOffenses) {
         // Method will handle printing the complete letter
+
     }
     public static void printLetterWithPrompts() {
         // Method will handle with printing the incomplete letter and will call the method "choices" to deal choices
-        String choice = choices();
+        String offense = offenseType();
+        String numberOfOffenses = numberOfOffense();
     }
 
-    public static String choices() {
-        // Method prints out the choices, and returns a String value
-        String userChoice;
+    public static String offenseType() {
+        // Method prints out the choices associated with offenses, and returns a String value
+        String offenseOption;
         byte option;
 
-        return userChoice;
+        return offenseOption;
+    }
+
+    public static String numberOfOffense() {
+        // Method prints out the choices associated with number of offenses, and returns a String value
+        String offenseNumber;
+        byte option;
+
+        return offenseNumber;
     }
 
     public static void main(String[] args) {
