562659394d603b35bd4837917cf3f972300fdb1e
jmusser28-psu <jmusser28@psu.edu>
2025-02-17 13:42:25 -0500

    Initial set up

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+    public static void printLetter() {
+        // Method will handle printing the complete letter
+    }
+    public static void printLetterWithPrompts() {
+        // Method will handle with printing the incomplete letter and will call the method "choices" to deal choices
+        String choice = choices();
+    }
+
+    public static String choices() {
+        // Method prints out the choices, and returns a String value
+        String userChoice;
+        byte option;
+
+        return userChoice;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        printLetter();
     }
 }
